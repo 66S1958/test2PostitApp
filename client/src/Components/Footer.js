244483695eaa0343b61b35744088239e378d445a
66S1958 <66S1958@utas.edu.om>
2024-12-04 14:47:31 +0400

    I put my father name in Footer.js

--- client/src/Components/Footer.js
+++ client/src/Components/Footer.js
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer className="footer">
-      <div>©2023.PostIT. All Rights Reserved. NoorAlhuda </div>
+      <div>©2023.PostIT. All Rights Reserved. NoorAlhuda Mohammed </div>
     </footer>
   );
 };
